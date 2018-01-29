d72d35eecde05013cabe739e8c4614f122abc5f5
hugotab <hugotabarin2@gmail.com>
2018-01-29 17:01:10 +0100

    sans faute

--- calculatrice/src/calculatrice/funtionCal.java
+++ calculatrice/src/calculatrice/funtionCal.java
@@ -3,10 +3,6 @@ public class funtionCal {
 	
 	int somA : Integer.parseInt (args[0]) ; 
 	int somB : Integer.parseInt (args[1]) ;
-	int somme = somA+somB; 
-
-	System.out.println("saisir deux resul: "); 
-
-
-	System.out.println("la somme : " somme); 
+	
+	int somme = somA+somB;  
 }
